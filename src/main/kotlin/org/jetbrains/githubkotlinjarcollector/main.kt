d9a977e48fd2dd46c3c4ffe253877413e1507836
Victor Petukhov <i@victor.am>
2018-02-28 00:31:34 +0300

    Changed argument key

--- src/main/kotlin/org/jetbrains/githubkotlinjarcollector/main.kt
+++ src/main/kotlin/org/jetbrains/githubkotlinjarcollector/main.kt
@@ -4,7 +4,7 @@ import com.xenomachina.argparser.ArgParser
 
 fun main(args : Array<String>) {
     val parser = ArgParser(args)
-    val packagesPath by parser.storing("-i", "--input", help="path to folder with package files (jar, jar zipped or apk)")
+    val packagesPath by parser.storing("-d", "--directory", help="path to folder with package files (jar, jar zipped or apk)")
     val stage by parser.mapping("--collecting" to Stage.COLLECTING, "--extracting" to Stage.EXTRACTING, help = "stage (--collecting or --extracting)")
 
     Runner.run(stage, packagesPath)
